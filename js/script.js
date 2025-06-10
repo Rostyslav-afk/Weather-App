2cf63a07488d348223fb9f10cbef970639db2436
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-06-10 10:52:14 +0200

    full css and openweahermap

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// https://api.openweathermap.org/data/2.5/weather?q=London&appid=e55f4249ad35c206cf3065236b7d3bc6&units=metric
+
+const apiKey = "";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e55f4249ad35c206cf3065236b7d3bc6&units=metric";

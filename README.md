91afb72022e2b894612944d72a230c4746763b26
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-06-10 13:20:18 +0200

    Full Project

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Weather-App
+# Weather App - Weather forecast in the World 🌤️
+
+**Weather App** - This is a simple and convenient service for viewing current weather in any city. Get accurate data in one click!
+
+## 🔹 Functionality
+- Current weather conditions (temperature, humidity, wind speed)
+- Search for 200,000+ cities
+- Responsive design (for PC and smartphones)
+
+## 🔹 Example data (Kyiv)
+- 23°C
+- Humidity: 45%
+- Wind speed: 0.89 km/h
+
+## 🔹 Technologies
+- API: OpenWeatherMap
+- Frontend: HTML5, CSS3, JavaScript
+
+## 🔹 How to use
+1. Enter the city name in the search
+2. Get a detailed forecast
+
+---
+🌍 **Available at:** (https://rostyslav-afk.github.io/Weather-App/) 
+📧 **Feedback:** goiteensschool@gmail.com
--- js/script.js
+++ js/script.js
@@ -1,4 +1,42 @@
 // https://api.openweathermap.org/data/2.5/weather?q=London&appid=e55f4249ad35c206cf3065236b7d3bc6&units=metric
 
-const apiKey = "";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e55f4249ad35c206cf3065236b7d3bc6&units=metric";
+const apiKey = "e55f4249ad35c206cf3065236b7d3bc6";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon");
+const weather = document.querySelector(".weather");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    let data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main === "Clouds") {
+        weatherIcon.src = "./images/clouds.png"
+    } else if (data.weather[0].main === "Clear") {
+        weatherIcon.src = "./images/clear.png"
+    } else if (data.weather[0].main === "rain") {
+        weatherIcon.src = "./images/rain.png"
+    } else if (data.weather[0].main === "Drizzle") {
+        weatherIcon.src = "./images/drizzle.png"
+    } else if (data.weather[0].main === "Mist") {
+        weatherIcon.src = "./images/mist.png"
+    }
+
+    weather.style.display = "block"
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+
+});
+
